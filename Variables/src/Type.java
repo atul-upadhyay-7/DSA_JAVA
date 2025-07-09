ca534cc5941d908086ec1490f5830857ca1a972f
Atul Upadhyay <atulupadhyay192@gmail.com>
2025-07-09 18:45:29 +0530

    commit

---
+++ Variables/src/Type.java
@@ -0,0 +1,47 @@
+//Implicit/widening/Type conversion
+
+import java.util.*;
+//public class Type{
+//    public static void main(String[] args){
+//    Scanner sc=new Scanner(System.in);
+//    float num=sc.nextInt();
+//    System.out.println(num);
+//    }
+//}
+
+//Type Casting/Explicit/narrowing
+
+//public class Type{
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        float number=99.999f;
+//        int number2=(int) number;
+//        System.out.println(number2);
+//    }
+//}
+
+//converting character  to integer
+//public class Type{
+//    public static void main(String[] args){
+//        char ch ='a';
+//        int number = ch;
+//        System.out.println(number);
+//    }
+//}
+
+//Type Promotion Expression
+public class Type{
+    public static void main(String[] args){
+//        byte b=5;
+//        byte a=(byte) (b*2);
+//        System.out.println(a);
+
+//        Another code
+        int a =10;
+        float b=20.25f;
+        long c=35;
+        double d=30;
+        double e=a+b+c+d;
+        System.out.println(e);
+    }
+}
---
+++ Variables/src/questions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class questions {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+//        Average of three numbers
+//        int a= sc.nextInt();
+//        int b= sc.nextInt();
+//        int c= sc.nextInt();
+//        int average =(a+b+c)/3;
+//        System.out.println(average);
+
+//        Area of the square
+//        int side=sc.nextInt();
+//        int area=side*side;
+//        System.out.println("The are of the Square is "+area);
+
+//        question
+//        float pencil = sc.nextFloat();
+//        float pen = sc.nextFloat();
+//        float eraser = sc.nextFloat();
+//        float total = pencil + pen + eraser;
+//        System.out.println("Bill is : " + total);
+//        float newTotal = total + (0.18f * total);
+//        System.out.println("Bill with 18% tax : " + newTotal);
+
+  }
+}
